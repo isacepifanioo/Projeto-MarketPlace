e2aa059b8cc83564692d6b2a2c0bf39abe157916
Isac Epifanio <isacepifanio4@gmail.com>
2025-02-11 14:07:05 -0300

    feat: setup frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <>
+      <h1>Ola</h1>
+      <input type="date" name="" id="" onChange={(e) => console.log(e.target.value)}/>
+    </>
+  )
+}
+
+export default App
